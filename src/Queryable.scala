2a8b7171e1b37a28229558dd7f19a1b47e78355b
Li Haoyi <haoyi.sg@gmail.com>
2023-10-02 21:28:05 +0800

    docstring for Queryable

--- src/Queryable.scala
+++ src/Queryable.scala
@@ -2,10 +2,14 @@ package usql
 
 import OptionPickler.{Reader, Writer}
 
-trait Queryable[T, V]{
-  def toTables(t: T): Set[Table.Base]
+/**
+ * Typeclass to indicate that we are able to evaluate a query of type [[Q]] to
+ * return a result of type [[V]]
+ */
+trait Queryable[Q, V]{
+  def toTables(t: Q): Set[Table.Base]
+  def queryWriter: Writer[Q]
   def valueReader: Reader[V]
-  def queryWriter: Writer[T]
 }
 
 object Queryable{
@@ -20,19 +24,19 @@ object Queryable{
     new Queryable.Simple[Expr[T], T](_.toTables, queryWriter0, valueReader0)
   }
 
-  implicit def tuple2Qr[E1, E2, T1, T2](implicit q1: Queryable[E1, T1],
-                                        q2: Queryable[E2, T2]): Queryable[(E1, E2), (T1, T2)] = {
-    new Queryable.Simple[(E1, E2), (T1, T2)](
+  implicit def tuple2Qr[Q1, Q2, V1, V2](implicit q1: Queryable[Q1, V1],
+                                        q2: Queryable[Q2, V2]): Queryable[(Q1, Q2), (V1, V2)] = {
+    new Queryable.Simple[(Q1, Q2), (V1, V2)](
       t => q1.toTables(t._1) ++ q2.toTables(t._2),
       OptionPickler.Tuple2Writer(q1.queryWriter, q2.queryWriter),
       OptionPickler.Tuple2Reader(q1.valueReader, q2.valueReader),
     )
   }
 
-  implicit def tuple3Qr[E1, E2, E3, T1, T2, T3](implicit q1: Queryable[E1, T1],
-                                                q2: Queryable[E2, T2],
-                                                q3: Queryable[E3, T3]): Queryable[(E1, E2, E3), (T1, T2, T3)] = {
-    new Queryable.Simple[(E1, E2, E3), (T1, T2, T3)](
+  implicit def tuple3Qr[Q1, Q2, Q3, V1, V2, V3](implicit q1: Queryable[Q1, V1],
+                                                q2: Queryable[Q2, V2],
+                                                q3: Queryable[Q3, V3]): Queryable[(Q1, Q2, Q3), (V1, V2, V3)] = {
+    new Queryable.Simple[(Q1, Q2, Q3), (V1, V2, V3)](
       t => q1.toTables(t._1) ++ q2.toTables(t._2) ++ q3.toTables(t._3),
       OptionPickler.Tuple3Writer(q1.queryWriter, q2.queryWriter, q3.queryWriter),
       OptionPickler.Tuple3Reader(q1.valueReader, q2.valueReader, q3.valueReader),
