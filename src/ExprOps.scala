5b172f75780fe9ef10361331ec4b73894856f5a4
Li Haoyi <haoyi.sg@gmail.com>
2023-10-07 15:36:29 +0800

    ExprIntOps -> ExprOps

---
+++ src/ExprOps.scala
@@ -0,0 +1,19 @@
+package usql
+
+import usql.SqlStr.SqlStringSyntax
+
+object ExprIntOps extends ExprIntOps
+trait ExprIntOps {
+  implicit class ExprIntOps0(v: Expr[Int]) {
+    def *(x: Int): Expr[Int] = Expr { implicit ctx => v.toSqlExpr + usql" * $x" }
+    def >(x: Int): Expr[Boolean] = Expr { implicit ctx => v.toSqlExpr + usql" > $x" }
+  }
+  implicit class ExprOps0(v: Expr[_]) {
+    def ===(x: Int): Expr[Boolean] = Expr { implicit ctx => v.toSqlExpr + usql" = $x" }
+    def ===(x: String): Expr[Boolean] = Expr { implicit ctx => v.toSqlExpr + usql" = $x" }
+    def ===(x: Expr[_]): Expr[Boolean] = Expr{ implicit ctx => v.toSqlExpr + usql" = " + x.toSqlExpr }
+  }
+  implicit class ExprBooleanOps0(v: Expr[Boolean]) {
+    def &&(x: Expr[Boolean]): Expr[Boolean] = Expr { implicit ctx => v.toSqlExpr + usql" AND " + x.toSqlExpr }
+  }
+}
--- src/Query.scala
+++ src/Query.scala
@@ -156,6 +156,11 @@ trait Expr[T] {
 }
 
 object Expr{
+  def apply[T](f: QueryToSql.Context => SqlStr): Expr[T] = new Simple[T](f)
+  class Simple[T](f: QueryToSql.Context => SqlStr) extends Expr[T]{
+    def toSqlExpr0(implicit ctx: QueryToSql.Context): SqlStr = f(ctx)
+  }
+
   def apply[T](x: T)(implicit conv: T => Interp) = new Expr[T] {
     override def toSqlExpr0(implicit ctx: QueryToSql.Context): SqlStr = new SqlStr(Seq("", ""), Seq(conv(x)), ())
   }
