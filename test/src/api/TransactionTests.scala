1fa69f7dcaead9f650dfeaba282b2ab03595240e
Li Haoyi <haoyi.sg@gmail.com>
2023-10-28 21:02:08 +0800

    two more transaction tests

--- test/src/api/TransactionTests.scala
+++ test/src/api/TransactionTests.scala
@@ -1,13 +1,25 @@
 package scaalsql.api
 
-import scalasql.{Buyer, Purchase}
+import scalasql.Purchase
 import scalasql.utils.ScalaSqlSuite
 import utest._
 
 trait TransactionTests extends ScalaSqlSuite{
-
+  override def utestBeforeEach(path: Seq[String]): Unit = checker.reset()
   def tests = Tests {
-    test("test") - {
+    test("commit") - {
+      checker.db.transaction{ implicit db =>
+        db.run(Purchase.select.size) ==> 7
+
+        db.run(Purchase.delete(_ => true)) ==> 7
+
+        db.run(Purchase.select.size) ==> 0
+      }
+
+      checker.db.autoCommit.run(Purchase.select.size) ==> 0
+    }
+
+    test("explicitRollback") - {
       checker.db.transaction{ implicit db =>
         db.run(Purchase.select.size) ==> 7
 
@@ -19,6 +31,24 @@ trait TransactionTests extends ScalaSqlSuite{
 
         db.run(Purchase.select.size) ==> 7
       }
+
+      checker.db.autoCommit.run(Purchase.select.size) ==> 7
+    }
+
+    test("throwRollback") - {
+      class FooException extends Exception
+      try checker.db.transaction{ implicit db =>
+        db.run(Purchase.select.size) ==> 7
+
+        db.run(Purchase.delete(_ => true)) ==> 7
+
+        db.run(Purchase.select.size) ==> 0
+
+        throw new FooException()
+
+      }catch{case e: FooException => /*donothing*/}
+
+      checker.db.autoCommit.run(Purchase.select.size) ==> 7
     }
   }
 }
