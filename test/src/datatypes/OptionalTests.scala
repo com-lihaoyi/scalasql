83e001c11d8d85cda02a1fcd0b274aa508790238
Li Haoyi <haoyi.sg@gmail.com>
2023-10-29 20:35:18 +0800

    more rigorous OptionalTests

--- test/src/datatypes/OptionalTests.scala
+++ test/src/datatypes/OptionalTests.scala
@@ -19,8 +19,13 @@ trait OptionalTests extends ScalaSqlSuite {
   def tests = Tests {
 
     checker(
-      query = OptCols.insert.batched(_.myInt, _.myInt2)((None, None), (Some(1), Some(2))),
-      value = 2
+      query = OptCols.insert.batched(_.myInt, _.myInt2)(
+        (None, None),
+        (Some(1), Some(2)),
+        (Some(3), None),
+        (None, Some(4)),
+      ),
+      value = 4
     )
 
     test("selectAll") - checker(
@@ -31,7 +36,12 @@ trait OptionalTests extends ScalaSqlSuite {
           opt_cols0.my_int2 as res__my_int2
         FROM opt_cols opt_cols0
       """,
-      value = Seq(OptCols[Id](None, None), OptCols[Id](Some(1), Some(2)))
+      value = Seq(
+        OptCols[Id](None, None),
+        OptCols[Id](Some(1), Some(2)),
+        OptCols[Id](Some(3), None),
+        OptCols[Id](None, Some(4))
+      )
     )
 
     test("groupByMaxGet") - checker(
@@ -42,9 +52,11 @@ trait OptionalTests extends ScalaSqlSuite {
         GROUP BY opt_cols0.my_int
       """,
       value = Seq(
-        None -> None,
-        Some(1) -> Some(2)
-      )
+        None -> Some(4),
+        Some(1) -> Some(2),
+        Some(3) -> None
+      ),
+      normalize = (x: Seq[(Option[Int], Option[Int])]) => x.sorted
     )
 
     test("isDefined") - checker(
@@ -55,7 +67,10 @@ trait OptionalTests extends ScalaSqlSuite {
           opt_cols0.my_int2 as res__my_int2
         FROM opt_cols opt_cols0
         WHERE opt_cols0.my_int IS NOT NULL""",
-      value = Seq(OptCols[Id](Some(1), Some(2)))
+      value = Seq(
+        OptCols[Id](Some(1), Some(2)),
+        OptCols[Id](Some(3), None),
+      )
     )
 
     test("isEmpty") - checker(
@@ -66,68 +81,77 @@ trait OptionalTests extends ScalaSqlSuite {
           opt_cols0.my_int2 as res__my_int2
         FROM opt_cols opt_cols0
         WHERE opt_cols0.my_int IS NULL""",
-      value = Seq(OptCols[Id](None, None))
+      value = Seq(
+        OptCols[Id](None, None),
+        OptCols[Id](None, Some(4)),
+      )
     )
 
-    test("sqlEqualsNonOptionHit") - checker(
-      query = OptCols.select.filter(_.myInt `=` 1),
-      sql = """
-        SELECT
-          opt_cols0.my_int as res__my_int,
-          opt_cols0.my_int2 as res__my_int2
-        FROM opt_cols opt_cols0
-        WHERE opt_cols0.my_int = ?
-      """,
-      value = Seq(OptCols[Id](Some(1), Some(2)))
-    )
+    test("sqlEquals"){
+      test("nonOptionHit") - checker(
+        query = OptCols.select.filter(_.myInt `=` 1),
+        sql = """
+          SELECT
+            opt_cols0.my_int as res__my_int,
+            opt_cols0.my_int2 as res__my_int2
+          FROM opt_cols opt_cols0
+          WHERE opt_cols0.my_int = ?
+        """,
+        value = Seq(OptCols[Id](Some(1), Some(2)))
+      )
 
-    test("sqlEqualsNonOptionMiss") - checker(
-      query = OptCols.select.filter(_.myInt `=` 2),
-      sql = """
-        SELECT
-          opt_cols0.my_int as res__my_int,
-          opt_cols0.my_int2 as res__my_int2
-        FROM opt_cols opt_cols0
-        WHERE opt_cols0.my_int = ?
-      """,
-      value = Seq[OptCols[Id]]()
-    )
+      test("nonOptionMiss") - checker(
+        query = OptCols.select.filter(_.myInt `=` 2),
+        sql = """
+          SELECT
+            opt_cols0.my_int as res__my_int,
+            opt_cols0.my_int2 as res__my_int2
+          FROM opt_cols opt_cols0
+          WHERE opt_cols0.my_int = ?
+        """,
+        value = Seq[OptCols[Id]]()
+      )
 
-    test("sqlEqualsOptionMiss") - checker( // SQL null = null is false
-      query = OptCols.select.filter(_.myInt `=` Option.empty[Int]),
-      sql = """
-        SELECT
-          opt_cols0.my_int as res__my_int,
-          opt_cols0.my_int2 as res__my_int2
-        FROM opt_cols opt_cols0
-        WHERE opt_cols0.my_int = ?
-      """,
-      value = Seq[OptCols[Id]]()
-    )
+      test("optionMiss") - checker( // SQL null = null is false
+        query = OptCols.select.filter(_.myInt `=` Option.empty[Int]),
+        sql = """
+          SELECT
+            opt_cols0.my_int as res__my_int,
+            opt_cols0.my_int2 as res__my_int2
+          FROM opt_cols opt_cols0
+          WHERE opt_cols0.my_int = ?
+        """,
+        value = Seq[OptCols[Id]]()
+      )
+    }
+    test("scalaEquals"){
+      test("someHit") - checker(
+        query = OptCols.select.filter(_.myInt === Option(1)),
+        sql = """
+          SELECT
+            opt_cols0.my_int as res__my_int,
+            opt_cols0.my_int2 as res__my_int2
+          FROM opt_cols opt_cols0
+          WHERE (opt_cols0.my_int IS NULL AND ? IS NULL) OR opt_cols0.my_int = ?
+        """,
+        value = Seq(OptCols[Id](Some(1), Some(2)))
+      )
 
-    test("scalaEqualsSomeHit") - checker(
-      query = OptCols.select.filter(_.myInt === Option(1)),
-      sql = """
-        SELECT
-          opt_cols0.my_int as res__my_int,
-          opt_cols0.my_int2 as res__my_int2
-        FROM opt_cols opt_cols0
-        WHERE (opt_cols0.my_int IS NULL AND ? IS NULL) OR opt_cols0.my_int = ?
-      """,
-      value = Seq(OptCols[Id](Some(1), Some(2)))
-    )
-
-    test("scalaEqualsNoneHit") - checker(
-      query = OptCols.select.filter(_.myInt === Option.empty[Int]),
-      sql = """
-        SELECT
-          opt_cols0.my_int as res__my_int,
-          opt_cols0.my_int2 as res__my_int2
-        FROM opt_cols opt_cols0
-        WHERE (opt_cols0.my_int IS NULL AND ? IS NULL) OR opt_cols0.my_int = ?
-      """,
-      value = Seq(OptCols[Id](None, None))
-    )
+      test("noneHit") - checker(
+        query = OptCols.select.filter(_.myInt === Option.empty[Int]),
+        sql = """
+          SELECT
+            opt_cols0.my_int as res__my_int,
+            opt_cols0.my_int2 as res__my_int2
+          FROM opt_cols opt_cols0
+          WHERE (opt_cols0.my_int IS NULL AND ? IS NULL) OR opt_cols0.my_int = ?
+        """,
+        value = Seq(
+          OptCols[Id](None, None),
+          OptCols[Id](None, Some(4)),
+        )
+      )
+    }
 
     test("map") - checker(
       query = OptCols.select.map(d => d.copy[Expr](myInt = d.myInt.map(_ + 10))),
@@ -137,13 +161,18 @@ trait OptionalTests extends ScalaSqlSuite {
         opt_cols0.my_int2 as res__my_int2
       FROM opt_cols opt_cols0
       """,
-      value = Seq(OptCols[Id](None, None), OptCols[Id](Some(11), Some(2)))
+      value = Seq(
+        OptCols[Id](None, None),
+        OptCols[Id](Some(11), Some(2)),
+        OptCols[Id](Some(13), None),
+        OptCols[Id](None, Some(4))
+      )
     )
 
     test("map2") - checker(
       query = OptCols.select.map(_.myInt.map(_ + 10)),
       sql = "SELECT opt_cols0.my_int + ? as res FROM opt_cols opt_cols0",
-      value = Seq(None, Some(11))
+      value = Seq(None, Some(11), Some(13), None)
     )
 
     test("flatMap") - checker(
@@ -155,7 +184,13 @@ trait OptionalTests extends ScalaSqlSuite {
           opt_cols0.my_int2 as res__my_int2
         FROM opt_cols opt_cols0
       """,
-      value = Seq(OptCols[Id](None, None), OptCols[Id](Some(13), Some(2)))
+      value = Seq(
+        OptCols[Id](None, None),
+        OptCols[Id](Some(13), Some(2)),
+        // because my_int2 is added to my_int, and my_int2 is null, my_int becomes null too
+        OptCols[Id](None, None),
+        OptCols[Id](None, Some(4))
+      )
     )
 
   }
