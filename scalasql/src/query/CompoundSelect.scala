86f9cce21c48392be26860859bc1948aa2c4c3f2
Li Haoyi <haoyi.sg@gmail.com>
2023-11-23 21:05:44 +0800

    use Select.Proxy in Values and CompoundSelect

--- scalasql/src/query/CompoundSelect.scala
+++ scalasql/src/query/CompoundSelect.scala
@@ -16,7 +16,7 @@ class CompoundSelect[Q, R](
     val limit: Option[Int],
     val offset: Option[Int]
 )(implicit val qr: Queryable.Row[Q, R])
-    extends Select[Q, R] {
+    extends Select.Proxy[Q, R] {
 
   protected def copy[Q, R](
       lhs: SimpleSelect[Q, R] = this.lhs,
@@ -25,17 +25,13 @@ class CompoundSelect[Q, R](
       limit: Option[Int] = this.limit,
       offset: Option[Int] = this.offset
   )(implicit qr: Queryable.Row[Q, R]) = newCompoundSelect(lhs, compoundOps, orderBy, limit, offset)
-  protected def expr = WithExpr.get(Joinable.joinableSelect(lhs))
+  override protected def expr = WithExpr.get(Joinable.joinableSelect(lhs))
 
   protected override def joinableSelect = this
 
-  def distinct: Select[Q, R] = selectSimpleFrom().distinct
   protected def selectSimpleFrom() = this.subquery
-  def queryExpr[V: TypeMapper](f: Q => Context => SqlStr)(
-      implicit qr: Queryable.Row[Expr[V], V]
-  ): Expr[V] = selectSimpleFrom().queryExpr[V](f)
 
-  def map[Q2, R2](f: Q => Q2)(implicit qr2: Queryable.Row[Q2, R2]): Select[Q2, R2] = {
+  override def map[Q2, R2](f: Q => Q2)(implicit qr2: Queryable.Row[Q2, R2]): Select[Q2, R2] = {
     (lhs, compoundOps) match {
       case (s: SimpleSelect[Q, R], Nil) =>
         val mapped = s.map(f)
@@ -45,11 +41,7 @@ class CompoundSelect[Q, R](
     }
   }
 
-  def flatMap[Q2, R2](f: Q => FlatJoin.Rhs[Q2, R2])(
-      implicit qr: Queryable.Row[Q2, R2]
-  ): Select[Q2, R2] = { selectSimpleFrom().flatMap(f) }
-
-  def filter(f: Q => Expr[Boolean]): Select[Q, R] = {
+  override def filter(f: Q => Expr[Boolean]): Select[Q, R] = {
     (lhs, compoundOps) match {
       case (s: SimpleSelect[Q, R], Nil) =>
         copy(Select.selectSimpleFrom(s.filter(f)), compoundOps, orderBy, limit, offset)
@@ -57,80 +49,42 @@ class CompoundSelect[Q, R](
     }
   }
 
-  def join0[Q2, R2](
-      prefix: String,
-      other: Joinable[Q2, R2],
-      on: Option[(Q, Q2) => Expr[Boolean]]
-  )(
-      implicit joinQr: Queryable.Row[Q2, R2]
-  ): Select[(Q, Q2), (R, R2)] = { selectSimpleFrom().join0(prefix, other, on) }
-
-  def leftJoin[Q2, R2](other: Joinable[Q2, R2])(on: (Q, Q2) => Expr[Boolean])(
-      implicit joinQr: Queryable.Row[Q2, R2]
-  ): Select[(Q, JoinNullable[Q2]), (R, Option[R2])] = { selectSimpleFrom().leftJoin(other)(on) }
-
-  def rightJoin[Q2, R2](other: Joinable[Q2, R2])(on: (Q, Q2) => Expr[Boolean])(
-      implicit joinQr: Queryable.Row[Q2, R2]
-  ): Select[(JoinNullable[Q], Q2), (Option[R], R2)] = { selectSimpleFrom().rightJoin(other)(on) }
-
-  def outerJoin[Q2, R2](other: Joinable[Q2, R2])(on: (Q, Q2) => Expr[Boolean])(
-      implicit joinQr: Queryable.Row[Q2, R2]
-  ): Select[(JoinNullable[Q], JoinNullable[Q2]), (Option[R], Option[R2])] = {
-    selectSimpleFrom().outerJoin(other)(on)
-  }
-
-  def aggregate[E, V](f: SelectProxy[Q] => E)(implicit qr: Queryable.Row[E, V]): Aggregate[E, V] = {
-    selectSimpleFrom().aggregate(f)
-  }
-
-  def mapAggregate[Q2, R2](
-      f: (Q, SelectProxy[Q]) => Q2
-  )(implicit qr: Queryable.Row[Q2, R2]): Select[Q2, R2] = {
-    selectSimpleFrom().mapAggregate(f)
-  }
-
-  def groupBy[K, V, R1, R2](groupKey: Q => K)(
-      groupAggregate: SelectProxy[Q] => V
-  )(implicit qrk: Queryable.Row[K, R1], qrv: Queryable.Row[V, R2]): Select[(K, V), (R1, R2)] = {
-    selectSimpleFrom().groupBy(groupKey)(groupAggregate)
-  }
-
-  def sortBy(f: Q => Expr[_]) = {
+  override def sortBy(f: Q => Expr[_]) = {
     val newOrder = Seq(OrderBy(f(expr), None, None))
 
     if (limit.isEmpty && offset.isEmpty) copy(orderBy = newOrder ++ orderBy)
     else newCompoundSelect(selectSimpleFrom(), compoundOps, newOrder, None, None)
   }
 
-  def asc =
+  override def asc =
     copy(orderBy = orderBy.take(1).map(_.copy(ascDesc = Some(AscDesc.Asc))) ++ orderBy.drop(1))
 
-  def desc =
+  override def desc =
     copy(orderBy = orderBy.take(1).map(_.copy(ascDesc = Some(AscDesc.Desc))) ++ orderBy.drop(1))
 
-  def nullsFirst =
+  override def nullsFirst =
     copy(orderBy = orderBy.take(1).map(_.copy(nulls = Some(Nulls.First))) ++ orderBy.drop(1))
 
-  def nullsLast =
+  override def nullsLast =
     copy(orderBy = orderBy.take(1).map(_.copy(nulls = Some(Nulls.Last))) ++ orderBy.drop(1))
 
-  def compound0(op: String, other: Select[Q, R]) = {
+  override def compound0(op: String, other: Select[Q, R]) = {
     val op2 = CompoundSelect.Op(op, Select.selectSimpleFrom(other))
     if (orderBy.isEmpty && limit.isEmpty && offset.isEmpty)
       copy(compoundOps = compoundOps ++ Seq(op2))
     else newCompoundSelect(selectSimpleFrom(), Seq(op2), Nil, None, None)
   }
 
-  def drop(n: Int) = copy(offset = Some(offset.getOrElse(0) + n), limit = limit.map(_ - n))
-  def take(n: Int) = copy(limit = Some(limit.fold(n)(math.min(_, n))))
+  override def drop(n: Int) = copy(offset = Some(offset.getOrElse(0) + n), limit = limit.map(_ - n))
+  override def take(n: Int) = copy(limit = Some(limit.fold(n)(math.min(_, n))))
 
-  protected def queryValueReader = OptionPickler.SeqLikeReader2(qr.valueReader(expr), implicitly)
+  override protected def queryValueReader = OptionPickler.SeqLikeReader2(qr.valueReader(expr), implicitly)
 
-  protected def selectRenderer(prevContext: Context) =
+  override protected def selectRenderer(prevContext: Context) =
     new CompoundSelect.Renderer(this, prevContext)
   protected override def queryTypeMappers() = qr.toTypeMappers(expr)
 
-  protected def selectLhsMap(prevContext: Context): Map[Expr.Identity, SqlStr] = {
+  override protected def selectLhsMap(prevContext: Context): Map[Expr.Identity, SqlStr] = {
     Select.selectLhsMap(lhs, prevContext)
   }
 }
--- scalasql/src/query/Values.scala
+++ scalasql/src/query/Values.scala
@@ -10,16 +10,13 @@ import scalasql.utils.OptionPickler
  * a [[Select]] query.
  */
 class Values[T: TypeMapper](val ts: Seq[T])(implicit val qr: Queryable.Row[Expr[T], T])
-    extends Select[Expr[T], T] {
+    extends Select.Proxy[Expr[T], T] {
   assert(ts.nonEmpty, "`Values` clause does not support empty sequence")
-  def queryExpr[V: TypeMapper](f: Expr[T] => Context => SqlStr)(
-      implicit qr: Queryable.Row[Expr[V], V]
-  ): Expr[V] = selectSimpleFrom().queryExpr(f)
 
   protected def selectSimpleFrom() = this.subquery
   val tableRef = new SubqueryRef(this, qr)
   protected def columnName = "column1"
-  protected val expr: Expr[T] = Expr { implicit ctx =>
+  override protected val expr: Expr[T] = Expr { implicit ctx =>
     val prefix = ctx.fromNaming.get(tableRef) match {
       case Some("") => sql""
       case Some(s) => SqlStr.raw(s) + sql"."
@@ -30,80 +27,13 @@ class Values[T: TypeMapper](val ts: Seq[T])(implicit val qr: Queryable.Row[Expr[
 
   override protected def queryWalkExprs(): Seq[(List[String], Expr[_])] = Seq(Nil -> expr)
 
-  protected def queryValueReader: OptionPickler.Reader[Seq[T]] =
+  override protected def queryValueReader: OptionPickler.Reader[Seq[T]] =
     implicitly[OptionPickler.Reader[Seq[T]]]
 
   override protected def queryTypeMappers(): Seq[TypeMapper[_]] = Seq(implicitly[TypeMapper[T]])
 
-  override def distinct: Select[Expr[T], T] = selectSimpleFrom().distinct
-
-  override def map[Q2, R2](f: Expr[T] => Q2)(implicit qr: Queryable.Row[Q2, R2]) =
-    selectSimpleFrom().map(f)
-
-  override def flatMap[Q2, R2](f: Expr[T] => FlatJoin.Rhs[Q2, R2])(
-      implicit qr: Queryable.Row[Q2, R2]
-  ) =
-    selectSimpleFrom().flatMap(f)
-
-  def filter(f: Expr[T] => Expr[Boolean]): Select[Expr[T], T] = selectSimpleFrom().filter(f)
-
-  def aggregate[E, V](f: SelectProxy[Expr[T]] => E)(implicit qr: Queryable.Row[E, V]) =
-    selectSimpleFrom().aggregate(f)
-
-  def mapAggregate[Q2, R2](
-      f: (Expr[T], SelectProxy[Expr[T]]) => Q2
-  )(implicit qr: Queryable.Row[Q2, R2]): Select[Q2, R2] = {
-    selectSimpleFrom().mapAggregate(f)
-  }
-
-  def groupBy[K, V, R2, R3](groupKey: Expr[T] => K)(
-      groupAggregate: SelectProxy[Expr[T]] => V
-  )(implicit qrk: Queryable.Row[K, R2], qrv: Queryable.Row[V, R3]): Select[(K, V), (R2, R3)] =
-    selectSimpleFrom().groupBy(groupKey)(groupAggregate)
-
-  def sortBy(f: Expr[T] => Expr[_]): Select[Expr[T], T] = selectSimpleFrom().sortBy(f)
-
-  override def asc: Select[Expr[T], T] = selectSimpleFrom().asc
-
-  override def desc: Select[Expr[T], T] = selectSimpleFrom().desc
-
-  override def nullsFirst: Select[Expr[T], T] = selectSimpleFrom().nullsFirst
-
-  override def nullsLast: Select[Expr[T], T] = selectSimpleFrom().nullsLast
-
-  override protected def compound0(
-      op: String,
-      other: Select[Expr[T], T]
-  ): CompoundSelect[Expr[T], T] = ???
-
-  override def drop(n: Int): Select[Expr[T], T] = selectSimpleFrom().drop(n)
-
-  override def take(n: Int): Select[Expr[T], T] = selectSimpleFrom().take(n)
-
   override protected def selectRenderer(prevContext: Context): Select.Renderer =
     new Values.Renderer[T](this)(implicitly, prevContext)
-
-  override def leftJoin[Q2, R2](other: Joinable[Q2, R2])(on: (Expr[T], Q2) => Expr[Boolean])(
-      implicit joinQr: Queryable.Row[Q2, R2]
-  ): Select[(Expr[T], JoinNullable[Q2]), (T, Option[R2])] =
-    selectSimpleFrom().leftJoin(other)(on)
-
-  override def rightJoin[Q2, R2](other: Joinable[Q2, R2])(on: (Expr[T], Q2) => Expr[Boolean])(
-      implicit joinQr: Queryable.Row[Q2, R2]
-  ): Select[(JoinNullable[Expr[T]], Q2), (Option[T], R2)] =
-    selectSimpleFrom().rightJoin(other)(on)
-
-  override def outerJoin[Q2, R2](other: Joinable[Q2, R2])(on: (Expr[T], Q2) => Expr[Boolean])(
-      implicit joinQr: Queryable.Row[Q2, R2]
-  ): Select[(JoinNullable[Expr[T]], JoinNullable[Q2]), (Option[T], Option[R2])] =
-    selectSimpleFrom().outerJoin(other)(on)
-
-  override protected def join0[Q2, R2](
-      prefix: String,
-      other: Joinable[Q2, R2],
-      on: Option[(Expr[T], Q2) => Expr[Boolean]]
-  )(implicit joinQr: Queryable.Row[Q2, R2]): Select[(Expr[T], Q2), (T, R2)] =
-    selectSimpleFrom().join0(prefix, other, on)
 
   override protected def selectLhsMap(prevContext: Context): Map[Expr.Identity, SqlStr] = {
     Map(Expr.exprIdentity(expr) -> SqlStr.raw(columnName))
