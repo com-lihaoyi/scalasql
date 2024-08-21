25c0b4b62f7230df7a5bf5175a16b2d1c882ab38
José Vergara <jlvertol@gmail.com>
2024-08-21 08:09:47 -0400

    Move the schema test to its own test suite

--- scalasql/test/src/query/SelectTests.scala
+++ scalasql/test/src/query/SelectTests.scala
@@ -41,23 +41,6 @@ trait SelectTests extends ScalaSqlSuite {
       """
     )
 
-    test("schema") - checker(
-      query = Text { Invoice.select },
-      sql = """
-        SELECT invoice0.id AS id, invoice0.total AS total, invoice0.vendor_name AS vendor_name
-        FROM otherschema.invoice invoice0
-      """,
-      value = Seq(
-        Invoice[Sc](id = 1, total = 150.4, vendor_name = "Siemens"),
-        Invoice[Sc](id = 2, total = 213.3, vendor_name = "Samsung"),
-        Invoice[Sc](id = 3, total = 407.2, vendor_name = "Shell")
-      ),
-      docs = """
-        If your table belongs to a schema other than the default schema of your database,
-        you can specify this in your table definition with table.schemaName
-      """
-    )
-
     test("filter") {
       test("single") - checker(
         query = Text { ShippingInfo.select.filter(_.buyerId `=` 2) },
