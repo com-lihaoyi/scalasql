ab34ba24b074bdb46657828b349c4d28112ca70d
Kien Dang <kiend@proton.me>
2024-09-06 16:29:02 +0800

    Add MSSQL test suite

---
+++ scalasql/test/src/dialects/MsSqlDialectTests.scala
@@ -0,0 +1,11 @@
+package scalasql.dialects
+
+import scalasql._
+import utest._
+import utils.MsSqlSuite
+
+trait MsSqlDialectTests extends MsSqlSuite {
+  def description = "Operations specific to working with Microsoft SQL Databases"
+
+  def tests = Tests {}
+}
