30b83a8eb938e6f7e7def7dceb6dad4fe898b7df
Kien Dang <kiend@proton.me>
2024-12-03 10:15:57 +0800

    Update due to internal API change

--- scalasql/src/dialects/MsSqlDialect.scala
+++ scalasql/src/dialects/MsSqlDialect.scala
@@ -133,6 +133,7 @@ object MsSqlDialect extends MsSqlDialect {
       new SimpleSelect(
         Table.metadata(t).vExpr(ref, dialectSelf).asInstanceOf[V[Expr]],
         None,
+        None,
         false,
         Seq(ref),
         Nil,
@@ -161,6 +162,7 @@ object MsSqlDialect extends MsSqlDialect {
     override def newSimpleSelect[Q, R](
         expr: Q,
         exprPrefix: Option[Context => SqlStr],
+        exprSuffix: Option[Context => SqlStr],
         preserveAll: Boolean,
         from: Seq[Context.From],
         joins: Seq[Join],
@@ -170,13 +172,14 @@ object MsSqlDialect extends MsSqlDialect {
         implicit qr: Queryable.Row[Q, R],
         dialect: scalasql.core.DialectTypeMappers
     ): scalasql.query.SimpleSelect[Q, R] = {
-      new SimpleSelect(expr, exprPrefix, preserveAll, from, joins, where, groupBy0)
+      new SimpleSelect(expr, exprPrefix, exprSuffix, preserveAll, from, joins, where, groupBy0)
     }
   }
 
   class SimpleSelect[Q, R](
       expr: Q,
       exprPrefix: Option[Context => SqlStr],
+      exprSuffix: Option[Context => SqlStr],
       preserveAll: Boolean,
       from: Seq[Context.From],
       joins: Seq[Join],
@@ -186,6 +189,7 @@ object MsSqlDialect extends MsSqlDialect {
       extends scalasql.query.SimpleSelect(
         expr,
         exprPrefix,
+        exprSuffix,
         preserveAll,
         from,
         joins,
