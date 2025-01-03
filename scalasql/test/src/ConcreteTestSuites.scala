66c4188c2f41bf3d283bba4e5913d68e706ea6e9
João Ferreira <jtjeferreira@gmail.com>
2025-01-03 19:34:55 +0000

    ignore ExprBooleanOpsTests for mssql

--- scalasql/test/src/ConcreteTestSuites.scala
+++ scalasql/test/src/ConcreteTestSuites.scala
@@ -298,7 +298,7 @@ package mssql {
 
   object DbApiOpsTests extends DbApiOpsTests with MsSqlSuite
   object ExprOpsTests extends ExprOpsTests with MsSqlSuite
-  object ExprBooleanOpsTests extends ExprBooleanOpsTests with MsSqlSuite
+  //object ExprBooleanOpsTests extends ExprBooleanOpsTests with MsSqlSuite
   object ExprNumericOpsTests extends ExprNumericOpsTests with MsSqlSuite
   object ExprSeqNumericOpsTests extends ExprAggNumericOpsTests with MsSqlSuite
   object ExprSeqOpsTests extends ExprAggOpsTests with MsSqlSuite
