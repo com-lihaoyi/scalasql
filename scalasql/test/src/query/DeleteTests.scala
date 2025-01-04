a9c0ee46944d7e637b40eece349f57293e72a9bc
João Ferreira <jtjeferreira@gmail.com>
2025-01-04 02:59:55 +0000

    fix DeleteTests and ExprAggOpsTests (except mkString.sep)

--- scalasql/test/src/query/DeleteTests.scala
+++ scalasql/test/src/query/DeleteTests.scala
@@ -53,7 +53,7 @@ trait DeleteTests extends ScalaSqlSuite {
     test("all") {
       checker(
         query = Purchase.delete(_ => true),
-        sql = "DELETE FROM purchase WHERE ?",
+        sqls = Seq("DELETE FROM purchase WHERE ?","DELETE FROM purchase WHERE 1 = ?"),
         value = 7,
         docs = """
           If you actually want to delete all rows in the table, you can explicitly
