55eeea7a1e2b0c15a0b19a0a474c77d07d17fdd9
NPCRUS <npcrus@gmail.com>
2025-02-10 15:09:41 +0100

    adjusted InsertSelect to support onConflict

--- scalasql/src/dialects/OnConflictOps.scala
+++ scalasql/src/dialects/OnConflictOps.scala
@@ -16,8 +16,8 @@ trait OnConflictOps {
 
   implicit def OnConflictableInsertSelect[V[_[_]], C, R, R2](
       query: InsertSelect[V, C, R, R2]
-  ): OnConflict[V[Expr], Int] = {
-    new OnConflict[V[Expr], Int](query, WithSqlExpr.get(query), query.table)
+  ): OnConflict[V[Column], Int] = {
+    new OnConflict[V[Column], Int](query, WithSqlExpr.get(query), query.table)
   }
 
 }
