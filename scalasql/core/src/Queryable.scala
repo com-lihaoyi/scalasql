f3bff4e4400c9d0f46bcebb3d8d4e18449389018
nikitaglushchenko <nikita.glushchenko@spendit.de>
2025-02-26 18:50:58 +0100

    adjust name

--- scalasql/core/src/Queryable.scala
+++ scalasql/core/src/Queryable.scala
@@ -68,7 +68,7 @@ object Queryable {
     var nulls = 0
     var nonNulls = 0
 
-    def restAreNulls(columnsCount: Int): Boolean = {
+    def consumeNulls(columnsCount: Int): Boolean = {
       val result = Range.inclusive(index + 1, index + columnsCount).forall { i =>
         r.getObject(i) == null
       }
@@ -151,7 +151,7 @@ object Queryable {
       def walkExprs(q: JoinNullable[Q]) = qr.walkExprs(q.get)
 
       def construct(args: Queryable.ResultSetIterator): Option[R] = {
-        if (args.restAreNulls(qr.walkLabels().length)) {
+        if (args.consumeNulls(qr.walkLabels().length)) {
           None
         } else {
           Option(qr.construct(args))
