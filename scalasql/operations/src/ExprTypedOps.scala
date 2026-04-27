fdf019a92b85ed65ede93e24af7c1a95815406a5
NPCRUS <npcrus@gmail.com>
2026-04-27 13:03:32 +0300

    strictness for ===

--- scalasql/operations/src/ExprTypedOps.scala
+++ scalasql/operations/src/ExprTypedOps.scala
@@ -2,6 +2,7 @@ package scalasql.operations
 
 import scalasql.core.Expr
 import scalasql.core.SqlStr.SqlStringSyntax
+import scalasql.operations.strict.TypeEqProxy
 
 import scala.reflect.ClassTag
 
@@ -14,7 +15,7 @@ class ExprTypedOps[T: ClassTag](v: Expr[T]) {
    * Translates to `IS NOT DISTINCT FROM` if both values are nullable,
    * otherwise translates to `=`
    */
-  def ===[V: ClassTag](x: Expr[V]): Expr[Boolean] = Expr { implicit ctx =>
+  def ===[V: ClassTag](x: Expr[V])(using TypeEqProxy[T, V]): Expr[Boolean] = Expr { implicit ctx =>
     (isNullable[T], isNullable[V]) match {
       case (true, true) => sql"($v IS NOT DISTINCT FROM $x)"
       case _ => sql"($v = $x)"
@@ -26,7 +27,7 @@ class ExprTypedOps[T: ClassTag](v: Expr[T]) {
    * Translates to `IS DISTINCT FROM` if both values are nullable,
    * otherwise translates to `<>`
    */
-  def !==[V: ClassTag](x: Expr[V]): Expr[Boolean] = Expr { implicit ctx =>
+  def !==[V: ClassTag](x: Expr[V])(using TypeEqProxy[T, V]): Expr[Boolean] = Expr { implicit ctx =>
     (isNullable[T], isNullable[V]) match {
       case (true, true) => sql"($v IS DISTINCT FROM $x)"
       case _ => sql"($v <> $x)"
