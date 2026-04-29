cb04b49254891595f47ad957b9a35a5310504743
NPCRUS <npcrus@gmail.com>
2026-04-29 19:22:41 +0300

    Option operation also on deprecation

--- scalasql/operations/src/TypeEq.scala
+++ scalasql/operations/src/TypeEq.scala
@@ -8,11 +8,20 @@ trait TypeEq[A, B]
 private lazy val typeEq: TypeEq[Unit, Unit] = new TypeEq {}
 
 object TypeEq {
-  // given StrictMode = new StrictMode {}
   given [A, B](using A =:= B): TypeEq[A, B] =
     typeEq.asInstanceOf[TypeEq[A, B]]
+}
+
+trait LowLevelDeprecated {
+  @deprecated(
+    "Comparison operators for optional types is deprecated due broken typesafety - you may get unexpected results due to null propagation. Consider .map, .filter combined with .isDefined / isEmpty methods available on Expr[Option[A]]"
+  )
   given [A, B](using A =:= B): TypeEq[Option[A], B] =
     typeEq.asInstanceOf[TypeEq[Option[A], B]]
+
+  @deprecated(
+    "Comparison operators for optional types is deprecated due broken typesafety - you may get unexpected results due to null propagation. Consider .map, .filter combined with .isDefined / isEmpty methods available on Expr[Option[A]]"
+  )
   given [A, B](using A =:= B): TypeEq[A, Option[B]] =
     typeEq.asInstanceOf[TypeEq[A, Option[B]]]
 
@@ -25,5 +34,9 @@ object TypeEq {
 
 trait unsafeEq {
   given [A, B]: TypeEq[A, B] = typeEq.asInstanceOf[TypeEq[A, B]]
+  given [A, B](using A =:= B): TypeEq[A, Option[B]] =
+    typeEq.asInstanceOf[TypeEq[A, Option[B]]]
+  given [A, B](using A =:= B): TypeEq[Option[A], B] =
+    typeEq.asInstanceOf[TypeEq[Option[A], B]]
 }
 object unsafeEq extends unsafeEq
